46c5f15de0e09e345a3277123f8546befa9a3791
@wwwxiangmin1122com <1099965786@qq.com>
2020-07-07 18:56:36 +0800

    775

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -237,6 +237,33 @@ export default {
   },
   props: [],
   methods: {
+
+    delet(id){
+      this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(() => {
+         
+this.$http.delete("http://127.0.0.1:8080/delet").then(()=>{
+  this.getlogList()
+})
+   
+   this.$message({
+            type: 'success',
+            message: '删除成功!'
+          });
+        })
+        .catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          })
+        })
+
+
+    },
     add(){
       this.adddialogFormVisible = true;
     },
